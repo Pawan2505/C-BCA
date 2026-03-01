15e778c694bc64f0e53958db9f839cd17414afd8
pawan2505 <pawanaktu@gmail.com>
2026-03-01 18:14:24 +0530

    cpp

--- intro.cpp
+++ intro.cpp
@@ -76,3 +76,71 @@
 
 //  }
 
+
+
+// Variable Naming Rules in C++ :
+// 1. A variable name must start with a letter (a-z, A-Z) or an underscore (_).
+
+//  Ex : int num; // valid variable name
+//       int _num; // valid variable name
+
+// 2. A variable name can contain letters, digits (0-9) and underscores (_), but it cannot contain spaces or special characters.
+
+//  Ex : int num1; // valid variable name
+//       int num_1; // valid variable name
+
+// int 12num; // invalid variable name
+
+// Note : we can not start a variable name with a digit, but we can use digits in the middle or at the end of the variable name.
+
+// 3. A variable name cannot be a reserved keyword in C++. Reserved keywords are predefined words that have special meaning in the language and cannot be used as variable names.
+
+//  Ex : int if; // invalid variable name
+//       int while; // invalid variable name
+/
+
+
+// keywords in C++ :
+// - Keywords are reserved words in C++ that have a predefined meaning and cannot be used as identifiers (such as variable names, function names, etc.). They are used to define the syntax and structure of the C++ language. Some common keywords in C++ include:
+// - int: used to declare integer variables
+// - float: used to declare floating-point variables
+// - double: used to declare double-precision floating-point variables
+// - char: used to declare character variables
+// - if: used for conditional statements
+// - else: used for alternative conditional statements
+// - for: used for loop statements
+// - while: used for loop statements
+// - return: used to return a value from a function
+// - void: used to indicate that a function does not return a value
+// - class: used to define a class in object-oriented programming
+// - struct: used to define a structure in C++
+// - namespace: used to define a namespace to avoid name conflicts
+// 32 keywords in C++11, 63 keywords in C++14, 84 keywords in C++17, 95 keywords in C++20.
+
+
+// 4. Variable names are case-sensitive, which means that uppercase and lowercase letters are treated as different characters. For example, "num" and "Num" would be considered two different variables.
+
+// 5. It is a good practice to use meaningful variable names that describe the purpose of the variable. This helps improve code readability and maintainability. For example, instead of using "x" as a variable name, it would be better to use "age" if the variable is intended to store a person's age.
+
+// 6. we can not use white space in variable names, but we can use underscores to separate words in a variable name. For example, "first_name" is a valid variable name, while "first name" is not valid.
+
+// Ex: first_name -> valid variable name
+//     first name -> invalid variable name
+
+
+// #include<iostream>
+// using namespace std;    
+
+// int main(){
+
+//     // int age = 25; 
+//     // cout<<"My age is : "<<age<<endl;
+//     // return 0;
+// }
+
+#include<iostream>
+using names std;
+
+int main(){
+    
+}
