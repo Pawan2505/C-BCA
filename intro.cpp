33f6498df13629b972e2c65ba80f526366806906
pawan2505 <pawanaktu@gmail.com>
2026-03-01 18:01:03 +0530

    cpp

---
+++ intro.cpp
@@ -0,0 +1,78 @@
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//    cout<<"Hello World";
+
+// }
+
+// What is Programming Language?
+// - Programing lang is set of instruction that is usd to communicate with pc.
+// iostream -> header file -> library
+// main() -> function or Entery point of program.
+// std -> cin, cout -> define
+// cout -> output print
+// cin -> user input
+// << -> insertion operator
+// >> -> extraction operator
+// ; -> statement terminator
+// #include -> preprocessor directive
+// using namespace std -> to avoid writing std:: before every cin and cout
+// comment -> single line comment
+
+// multiline comment :
+/*
+   This is a multiline comment
+   It can span multiple lines
+   It is used to provide detailed explanations or to comment out blocks of code
+
+*/
+
+
+
+// #include<iostream>
+
+// // using namespace std;
+
+// int main()
+// {
+//    std::cout<<"Hello World"<<std::endl;
+//    std::cout<<"Hey Pawan";
+
+// }
+
+
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     cout<<"Hey Pawan";
+//     cout<<"Hello World"<<endl;
+
+// }
+
+
+// endl -> end line -> it is used to move the cursor to the next line after printing the output. It is equivalent to \n in C.
+// cpp is build by upgrading c programming language. It is a high level programming language that supports object oriented programming, procedural programming and generic programming. It is widely used for developing system software, application software, game development and many more.
+
+// :: -> scope resolution operator -> it is used to access the members of a namespace, class or struct. It is also used to define the member functions of a class outside the class definition.
+
+
+
+// Variable in C++ : 
+ // - A variable is a named storage location in memory that can hold a value of a specific data type. It is used to store and manipulate data in a program. In C++, variables must be declared before they can be used, and they can be initialized with a value at the time of declaration or assigned a value later in the program. Variables can be of various data types such as int, float, double, char, string, etc.
+
+
+//  #include<iostream>
+//  using namespace std;
+
+//  int main(){
+//     int num = 10; // declaration of variable
+//     cout<<"The value of num : "<<num<<endl;
+
+//  }
+
